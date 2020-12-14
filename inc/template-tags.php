dbe5e86b6da18a7c9f4dd61a931937b45284ef10
Kamila <kamila.dynysiuk@gmail.com>
2020-12-14 02:37:19 +0000

    update

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -45,6 +45,38 @@ if (!function_exists('act_outs_read_time')) :
 	}
 endif;
 
+/**
+ * Returns true if a blog has more than 1 category.
+ *
+ * @return bool
+ */
+function act_outs_categorized_blog()
+{
+	if (false === ($all_the_cool_cats = get_transient('act_outs_categories'))) {
+		// Create an array of all the categories that are attached to posts.
+		$all_the_cool_cats = get_categories(array(
+			'fields'     => 'ids',
+			'hide_empty' => 1,
+			// We only need to know if there is more than one category.
+			'number'     => 2,
+		));
+
+		// Count the number of categories that are attached to the posts.
+		$all_the_cool_cats = count($all_the_cool_cats);
+
+		set_transient('act_outs_categories', $all_the_cool_cats);
+	}
+
+	if ($all_the_cool_cats > 1) {
+		// This blog has more than 1 category so act_outs_categorized_blog should return true.
+		return true;
+	} else {
+		// This blog has only 1 category so act_outs_categorized_blog should return false.
+		return false;
+	}
+}
+
+
 
 if (!function_exists('act_outs_posted_on')) :
 	/**
