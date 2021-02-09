5ffd96d43ec68c377aba63322b4534188155e15e
Kamila <kamila.dynysiuk@gmail.com>
2021-02-09 21:43:44 +0000

    add header content

---
+++ template-parts/content-header-group.php
@@ -0,0 +1,4 @@
+<header class="group-header">
+    <p>Welcome to Act Outs Online and thank you for joining our group. Below you will find all the videos and materials needed for this term. If you have any questions, do not hestate to contact us as we will get back to you as soon as possible.</p>
+    <p> Thank you again and here is looking to a creative and fun term.</p>
+</header>
